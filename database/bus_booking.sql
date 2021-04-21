386c10207acbf12a8888ec34491e25ddac6a108e
itszakjo <itszakjo@gmail.com>
2021-04-21 14:22:54 +0200

    Improving the project structure using Service container

--- database/bus_booking.sql
+++ database/bus_booking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2021 at 08:34 PM
+-- Generation Time: Apr 18, 2021 at 02:51 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -29,13 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `booked_seats` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `ref_seat` int(11) NOT NULL,
+  `ref_seat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ref_trip` int(11) NOT NULL,
-  `ref_start_point` int(11) NOT NULL,
-  `ref_end_point` int(11) NOT NULL,
+  `ref_pickup_point` int(11) NOT NULL,
+  `ref_drop_point` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `booked_seats`
+--
+
+INSERT INTO `booked_seats` (`id`, `ref_seat`, `ref_trip`, `ref_pickup_point`, `ref_drop_point`, `created_at`, `updated_at`) VALUES
+(1, 'A1', 2, 4, 7, '2021-04-18 09:51:23', '2021-04-18 09:51:23'),
+(3, 'A2', 2, 4, 6, NULL, NULL),
+(4, 'A3', 2, 7, 8, '2021-04-18 10:21:31', '2021-04-18 10:21:31');
 
 -- --------------------------------------------------------
 
@@ -61,7 +70,8 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `name`, `trip`, `pickup_point`, `drop_point`, `seats`, `booking_date`, `total_price`, `created_at`, `updated_at`) VALUES
-(1, 'Zed Manager', 2, 4, 6, 'a_2, a_3, b_3,', '2021-04-17', 600.00, '2021-04-17 16:32:32', '2021-04-17 16:32:32');
+(1, 'Mahmoud Hesham', 2, 4, 7, 'A1, A2,', '2021-04-18', 400.00, '2021-04-18 09:51:23', '2021-04-18 09:51:23'),
+(2, 'Mahmoud Hesham', 2, 7, 8, 'A3,', '2021-04-18', 200.00, '2021-04-18 10:21:31', '2021-04-18 10:21:31');
 
 -- --------------------------------------------------------
 
@@ -101,11 +111,10 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2021_04_16_154712_create_stations_table', 1),
 (5, '2021_04_16_181928_create_trips_table', 1),
-(6, '2021_04_16_181949_create_stop_points_table', 1),
-(7, '2021_04_16_182717_create_bookings_table', 1),
-(8, '2021_04_17_101021_create_seats_table', 1),
-(9, '2021_04_17_101135_create_booked_seats_table', 1),
-(10, '2021_04_17_115811_create_routes_table', 2);
+(6, '2021_04_16_182717_create_bookings_table', 1),
+(7, '2021_04_17_101021_create_seats_table', 1),
+(8, '2021_04_17_101135_create_booked_seats_table', 1),
+(9, '2021_04_17_115811_create_routes_table', 2);
 
 -- --------------------------------------------------------
 
@@ -156,6 +165,24 @@ CREATE TABLE `seats` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `seats`
+--
+
+INSERT INTO `seats` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'A1', NULL, NULL),
+(2, 'A2', NULL, NULL),
+(3, 'A3', NULL, NULL),
+(4, 'A4', NULL, NULL),
+(5, 'B1', NULL, NULL),
+(6, 'B2', NULL, NULL),
+(7, 'B3', NULL, NULL),
+(8, 'B4', NULL, NULL),
+(9, 'C1', NULL, NULL),
+(10, 'C2', NULL, NULL),
+(11, 'C3', NULL, NULL),
+(12, 'C4', NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -182,20 +209,6 @@ INSERT INTO `stations` (`id`, `name`, `created_at`, `updated_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stop_points`
---
-
-CREATE TABLE `stop_points` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` int(11) NOT NULL,
-  `ref_trip` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `trips`
 --
 
@@ -216,8 +229,8 @@ CREATE TABLE `trips` (
 --
 
 INSERT INTO `trips` (`id`, `name`, `route`, `date`, `departure_at`, `arrival_at`, `seat_price`, `created_at`, `updated_at`) VALUES
-(2, 'New Trip to Asyut', 1, '2025-04-01', '07:03:00', '18:01:00', 200.00, '2021-04-17 12:27:22', '2021-04-17 12:34:10'),
-(4, 'fay - asu', 3, '2023-02-01', '14:00:00', '02:00:00', 55.00, '2021-04-17 12:35:07', '2021-04-17 12:35:07');
+(2, 'New Trip from Cairo to Asyut', 1, '2025-04-01', '07:03:00', '18:01:00', 200.00, '2021-04-17 12:27:22', '2021-04-17 21:57:07'),
+(4, 'from Fayoum to Asyut', 3, '2023-02-01', '14:00:00', '02:00:00', 55.00, '2021-04-17 12:35:07', '2021-04-17 21:57:26');
 
 -- --------------------------------------------------------
 
@@ -297,12 +310,6 @@ ALTER TABLE `stations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `stop_points`
---
-ALTER TABLE `stop_points`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indexes for table `trips`
 --
 ALTER TABLE `trips`
@@ -323,13 +330,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `booked_seats`
 --
 ALTER TABLE `booked_seats`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -353,19 +360,13 @@ ALTER TABLE `routes`
 -- AUTO_INCREMENT for table `seats`
 --
 ALTER TABLE `seats`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `stations`
 --
 ALTER TABLE `stations`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `stop_points`
---
-ALTER TABLE `stop_points`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `trips`
